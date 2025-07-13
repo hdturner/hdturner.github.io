a10f625afd7722a1681e81c3ffc56706997e6929
hdturner <hdturner@my.waketech.edu>
2025-07-13 11:10:44 -0400

    Moved gradebook files to root and committed for GitHub Pages

---
+++ gradebook.js
@@ -0,0 +1,15 @@
+// TODO: Fetch data from the PostgreSQL database (to be implemented later)
+function fetchGradeData() {
+    console.log("Fetching grade data...");
+}
+
+// TODO: Populate the table with grade data
+function populateGradebook(data) {
+    console.log("Populating gradebook with data", data);
+}
+
+// TODO REMOVE THIS
+// Call the stubs to demonstrate the workflow
+const gradeData = fetchGradeData();
+populateGradebook(gradeData);
+// END REMOVE
